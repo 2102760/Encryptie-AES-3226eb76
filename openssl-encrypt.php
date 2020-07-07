cc2598a995f4e20eef1b15fda35810dd995c7f61
Beau van Rouwendal <beauvanrouwendal@gmail.com>
2020-07-07 12:24:57 +0200

    Blast <3

--- openssl-encrypt.php
+++ openssl-encrypt.php
@@ -4,7 +4,7 @@ $plaintext = "Hallo";
 $cipher = "aes-128-gcm";
 $key = "afs";
 $ivlen = openssl_cipher_iv_length($cipher);
-$iv = openssl_random_pseudo_bytes( $ivlen);
+$iv = openssl_random_pseudo_bytes($ivlen);
 $encrypt = openssl_encrypt($plaintext, $cipher, $key, $options = 0, $iv, $tag);
 echo "Jou encryptie is:" . PHP_EOL;
 echo $encrypt . PHP_EOL;
